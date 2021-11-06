9425a63bc54315b27c0c023b9039df13fc14e649
Ben Keys <bkeys818@gmail.com>
2021-11-06 11:09:19 -0400

    🔧 Fix webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 /** @type { import('webpack').Configuration } */
 module.exports = {
-    mode: process.env.NODE_ENV,
-    entry: path.join(__dirname, 'src', 'index.js'),
+    entry: path.join(__dirname, 'src', 'index.tsx'),
     output: {
         path: path.join(__dirname, 'build'),
         filename: 'index.bundle.js',
     },
     mode: process.env.NODE_ENV || 'development',
     resolve: {
+        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
         extensions: ['.tsx', '.ts', '.js'],
     },
-    devServer: { contentBase: path.join(__dirname, 'src') },
     module: {
         rules: [
             {
@@ -25,7 +24,15 @@ module.exports = {
                 test: /\.(ts|tsx)$/,
                 exclude: /node_modules/,
                 use: ['ts-loader'],
-            }
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: ['file-loader'],
+            },
         ],
     },
     plugins: [
